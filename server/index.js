4e339f28071a0774ad6df552bdfd4e28dec917d6
Salio <hugo.saulig@outlook.com>
2026-02-02 15:33:33 +0100

    Fix Express 5 wildcard route syntax

--- server/index.js
+++ server/index.js
@@ -172,7 +172,7 @@ app.delete("/api/tasks/:id", requireAuth, async (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+app.get('*splat', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
